e0c2ce76b8a2e5b6eb07dac6badea11f366db204
quarkusio <no-reply@quarkus.io>
2020-05-07 00:56:43 +0000

    Initial commit

---
+++ src/test/java/co/pica/droptable/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package co.pica.droptable;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
